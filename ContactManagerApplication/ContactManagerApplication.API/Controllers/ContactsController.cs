e885393326329348659819f88754118c13003884
Alina Kyka <alinakyka2001@gmail.com>
2026-02-07 17:31:55 +0200

    Fix contacts service. Add client logic.

--- ContactManagerApplication/ContactManagerApplication.API/Controllers/ContactsController.cs
+++ ContactManagerApplication/ContactManagerApplication.API/Controllers/ContactsController.cs
@@ -4,20 +4,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ContactManagerApplication.API.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ContactsController : ControllerBase
+    public class ContactsController : Controller
     {
         private readonly IContactService _contactService;
-        private readonly ICsvService<ContactModel> _csvService;
+        private const int PAGE_SIZE = 20;
 
-        public ContactsController(IContactService contactService, ICsvService<ContactModel> csvService)
+        public ContactsController(IContactService contactService)
         {
             _contactService = contactService;
-            _csvService = csvService;
         }
 
-        [HttpPost("Csv")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var contacts = await _contactService.GetAllAsync(0, PAGE_SIZE, CancellationToken.None);
+
+            return View(contacts);
+        }
+
+        [HttpPost("contacts/csv")]
         [Consumes("multipart/form-data")]
         public async Task<ActionResult> UploadCsv(IFormFile file, CancellationToken ct = default)
         {
@@ -26,13 +31,27 @@ namespace ContactManagerApplication.API.Controllers
 
             try
             {
-                await _contactService.SaveContactsToDbAsync(_csvService.ImportEntitiesFromCsvAsync(file.OpenReadStream(), ct), ct);
+                await _contactService.ImportContactsAndSaveToDbAsync(file.OpenReadStream(), ct);
                 return Ok();
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
+        }
+
+        [HttpPut("contacts/{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] ContactModel model, CancellationToken ct = default)
+        {
+            await _contactService.UpdateAsync(id, model, ct);
+            return Ok();
+        }
+
+        [HttpDelete("contacts/{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id, CancellationToken ct = default)
+        {
+            await _contactService.DeleteAsync(id, ct);
+            return Ok();
         }
     }
 }
---
+++ ContactManagerApplication/ContactManagerApplication.Application/Models/ContactViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace ContactManagerApplication.Application.Models;
+public record ContactViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    [Name("Date of birth")]
+    public DateTime DateOfBirth { get; set; }
+    public bool Married { get; set; }
+    public string Phone { get; set; }
+    public decimal Salary { get; set; }
+}
